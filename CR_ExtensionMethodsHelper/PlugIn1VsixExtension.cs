b25d44251b237c501126393b74c46e7742cb767b
Rory Becker <RoryBecker@gmail.com>
2014-06-17 11:45:52 +0100

    Initial Load

---
+++ CR_ExtensionMethodsHelper/PlugIn1VsixExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.Composition;
+using DevExpress.CodeRush.Common;
+
+namespace CR_ExtensionMethodsHelper
+{
+    [Export(typeof(IVsixPluginExtension))]
+    public class CR_ExtensionMethodsHelperExtension : IVsixPluginExtension { }
+}
